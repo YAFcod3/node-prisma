642ed8e16f962bb543b7db4a1a92fafd7c6fa202
YAFcod3 <yasmaniarmas1995@gmail.com>
2023-08-13 00:38:30 -0400

    1er commit

---
+++ server/index.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import productsRoutes from './routes/products.routes.js'
+import categoriesRoutes from './routes/products.routes.js'
+
+
+const app= express()
+
+
+app.use(express.json())
+
+
+app.use('/api',productsRoutes)
+app.use('/api',categoriesRoutes )
+
+
+
+app.listen(3000)
+console.log('server is running on port 3000')
